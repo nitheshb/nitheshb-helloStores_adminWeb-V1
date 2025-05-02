6756a05363a2e8a66261b2ae218e78ab98841055
AnilKumar043 <anilkumar@ensarsolutions.com>
2025-05-02 15:11:44 +0530

    CRUD Operations on Unit table.

--- src/services/unit.js
+++ src/services/unit.js
@@ -1,31 +1,9 @@
 import request from './request';
-import { getAllUnits, getAllUnitsSnap, getAllUnitsById, createUnitsDb, updateUnits, deleteUnits } from './dbQueries/q_unit';
-import { update } from 'lodash'; 
+import { getAllUnits, getAllUnitsById, createUnitsDb, updateUnits, deleteUnits, setActiveUnits } from './dbQueries/q_unit';
 
-
-// const unitService = {
-//   getAll: (params) => request.get('dashboard/admin/units/paginate', { params }),
-//   // getById: (id, params) =>
-//   //   request.get(`dashboard/admin/units/${id}`, { params }),
-//   // create: (params) => request.post('dashboard/admin/units', {}, { params }),
-//   // update: (id, params) =>
-//   //   request.put(`dashboard/admin/units/${id}`, {}, { params }),
-//   // delete: (params) =>
-//   //   request.delete(`dashboard/admin/units/delete`, { params }),
-//   setActive: (id) => request.post(`dashboard/admin/units/active/${id}`),
-//   dropAll: () => request.get(`dashboard/admin/units/drop/all`),
-//   restoreAll: () => request.get(`dashboard/admin/units/restore/all`),
-//   // new
-// //  getAll: (params) => getAllUnits('spark', { params }),
-
-//   getById: (id, params) => getAllUnitsById('spark', id, { params }),
-//   create: (params) => createUnitsDb('spark', { params }),
-//   update: (id, params) => updateUnits( id,params ),
-//   delete: (params) => deleteUnits(params),  
-// };
 
 const unitService = {
-  getAll: (params) => request.get('dashboard/admin/units/paginate', { params }),
+  // getAll: (params) => request.get('dashboard/admin/units/paginate', { params }),
   // getById: (id, params) =>
   //   request.get(`dashboard/admin/units/${id}`, { params }),
   // create: (params) => request.post('dashboard/admin/units', {}, { params }),
@@ -33,12 +11,12 @@ const unitService = {
   //   request.put(`dashboard/admin/units/${id}`, {}, { params }),
   // delete: (params) =>
   //   request.delete(`dashboard/admin/units/delete`, { params }),
-  setActive: (id) => request.post(`dashboard/admin/units/active/${id}`),
+  // setActive: (id) => request.post(`dashboard/admin/units/active/${id}`),
   dropAll: () => request.get(`dashboard/admin/units/drop/all`),
   restoreAll: () => request.get(`dashboard/admin/units/restore/all`),
   // new
-//  getAll: (params) => getAllUnits('spark', { params }),
-
+ getAll: (params) => getAllUnits('spark', { params }),
+ setActive: (id) => setActiveUnits(`dashboard/admin/units/active/${id}`),
   getById: (id, params) => getAllUnitsById('spark', id, { params }),
   create: (params) => createUnitsDb('spark', { params }),
   update: (id, params) => updateUnits( id,params ),
@@ -46,17 +24,17 @@ const unitService = {
 };
 
 
-// const unitService = {
-//   getAll: (params) =>
-//     request.get('dashboard/admin/units/paginate', { params }),
-//   getById: (id) => request.get(`dashboard/admin/units/${id}`),
-//   create: (data) => request.post('dashboard/admin/units', data),
-//   update: (id, data) => request.put(`dashboard/admin/units/${id}`, data),
-//   delete: (params) =>
-//     request.delete(`dashboard/admin/units/delete`, { params }),
-//   setActive: (id) =>
-//     request.post(`dashboard/admin/units/${id}/active/status`),
-  
-// };
 
 export default unitService;
+
+
+
+
+
+
+
+
+
+
+
+
